d60f71594a522bed61e19dab10e3212a8bc2cb03
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-30 17:48:10 +0100

    refactor code and minor changes in func
    	* move some constants from the global scope
    	* create new constants for wordClasses and relationTypes
    	* use new constants to simplify func for diplaying relatedwords
    	* move fetch methode to seperat function make more reusable
    	* move func to determine searchterm to separet function
    	* move and simplyfy func for checking if which wordclasses
    	  display related words from
    	* minor row deletions and editions for consistensy
    	* add coments

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-//api_key=e42fbfbee78372e658de2b9941176e5d  flicker apikey
-
-//  Big Huge Thesaurus api key  http://words.bighugelabs.com/api/2/9b7c8ee5f8778f4292b2aebad9fed742/word/json
-
-const searchButton = document.querySelector('button');
-const searchField = document.querySelector('input');
-const content = document.querySelector('.content');
-const list = document.querySelector('ul');
-const flickerUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search";
-const flickerApiKey = "2e0240e78ddd71b7a1fb75ec2a28b41c";
-const flickerApendage = "safe_search=1&format=json&nojsoncallback=1";
-const bigHugeUrl = "http://words.bighugelabs.com/api/2/";
-const bigHugeApiKey = "9b7c8ee5f8778f4292b2aebad9fed742";
-let results;
+const searchButton = document.querySelector('button'); //button to trigger search
+const searchField = document.querySelector('input'); // input field where user types in searchwords
+const content = document.querySelector('.content'); // container where images will be displayed
+const list = document.querySelector('ul'); //list where related search word will be stored
 
 const appendListItem = (wordList) => {
 
   for (let j = 0; j < wordList.length; j++) {
+
     let listItem = document.createElement('li');
     let link = document.createElement('a');
 
@@ -23,88 +14,121 @@ const appendListItem = (wordList) => {
     listItem.firstChild.addEventListener('click', search);
     list.appendChild(listItem);
   }
-};
+}; //appends words from an array to the list and attaches a link to enable searching by clickin on the list items
 
-const checkForWordType = (wordType) => {
-  
-  if(wordType.syn) {
-    appendListItem(wordType.syn);
-  } else if(wordType.ant) {
-    appendListItem(wordType.ant);
-  } else if(wordType.usr) {
-    appendListItem(wordType.usr);
-  }
+const checkForWordType = (wordClass) => {
 
-};
+  const relationTypes = ["syn","sim", "ant","usr", "rel"];
+
+  relationTypes.forEach(type => {
+
+    if (wordClass[type]) {
+
+      appendListItem(wordClass[type]);
+    }
+  });
+}; //loops thrue all the possible realtiontyps there are on the bighugelabs api and runs the append functions if givven object has the type as an key
+
+const checkForWordClass = (res) => {
+
+  const wordClasses = ["verb", "adjective", "noun", "adverb", "pronoun"];
+
+  wordClasses.forEach(wordClass => {
+    
+    if (res[wordClass]) {
+
+      checkForWordType(res[wordClass]);
+    }     
+  });
+}; // loops thure some apropriet wordclasses and runs checkforwordtype function if incomping object has key that matches the curent word class
 
 const removeContent = (container) => {
+
   while(container.hasChildNodes()) {
+
     container.removeChild(container.firstChild);
   }
-};
+}; // removes all images from the container
 
-const displayResults = (results) => {
+const displayResults = (res) => {
 
-  for (let i = 0; i < results.length; i++) {
+  for (let i = 0; i < res.length; i++) {
     
     let figure = document.createElement('figure');
     let link = document.createElement('a');
     let img = document.createElement('img');
 
-    link.target = "_blank"
-    link.href = `https://www.flickr.com/photos/${results[i].owner}/${results[i].id}`;
-    img.src = `https://farm${results[i].farm}.staticflickr.com/${results[i].server}/${results[i].id}_${results[i].secret}.jpg`;
+    link.target = "_blank";
+    link.href = `https://www.flickr.com/photos/${res[i].owner}/${res[i].id}`;
+    img.src = `https://farm${res[i].farm}.staticflickr.com/${res[i].server}/${res[i].id}_${res[i].secret}.jpg`;
 
     link.appendChild(img);
     figure.appendChild(link);
     content.appendChild(figure);
   }
-};
+}; // creates new elements with the data from the search ressult and apends to the container
 
-const search = function(e) {
-  e.preventDefault();
+const fetchAndDisplayData = (completeUrl) => {
 
-  let searchValue;
-
-  if(searchField.value) {
-    searchValue = searchField.value;
-  } else {
-    searchValue = this.innerHTML;
-  }
-
-  removeContent(content);
-
-  fetch(`${flickerUrl}&api_key=${flickerApiKey}&text=${searchValue}&${flickerApendage}`)
-    .then(res => res.json()).then(res => {
-    
-      results = res.photos.photo;
-
-      displayResults(results);
-
-      searchField.value = "";
-     
-   
-    }).catch(e => {
-      console.log(e);
-    });
-
-  fetch(`${bigHugeUrl}${bigHugeApiKey}/${searchValue}/json`)
+  fetch(completeUrl)
     .then(res => res.json())
     .then(res => {
 
-      removeContent(list);
+      if(completeUrl.includes("api.flickr.com")) {
 
-      if (res.noun) {
-        checkForWordType(res.noun);
-        
-      } else if (res.verb) {
-        checkForWordType(res.verb);
-      }
+        res = res.photos.photo;
+
+        removeContent(content);
+        displayResults(res);  
+
+      } else {
+
+        removeContent(list);
+        checkForWordClass(res);
+      } /*this works becous there are only cales made to two diffrent api's if more where called 
+      adisional else if would be nacesary to preform the corect actions if they are not the same as for the big huges api*/
      
     }).catch(e => {
+
       console.log(e);
     });
-};
+}; // makes an async api call to fetch data from given api
 
+const determineSearchValue = (self) => {
+
+  let value;
+
+  if (searchField.value) {
+    
+    value = searchField.value;
+
+    searchField.value = "";
+
+  } else {
+
+    value = self.innerHTML;
+  }
+
+  return value;
+}; // determins if seatchfield has a value (if not search has been made by clicking on a link) and returns corect value and emptys search field if filld
+
+const search = function(e) {
+
+  e.preventDefault();
+
+  const flickerUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search";
+  const flickerApiKey = "2e0240e78ddd71b7a1fb75ec2a28b41c";
+  const flickerApendage = "safe_search=1&format=json&nojsoncallback=1";
+  const bigHugeUrl = "http://words.bighugelabs.com/api/2/";
+  const bigHugeApiKey = "9b7c8ee5f8778f4292b2aebad9fed742";
+
+  let searchTerm = determineSearchValue(this);
+
+  fetchAndDisplayData(`${flickerUrl}&api_key=${flickerApiKey}&text=${searchTerm}&${flickerApendage}`);
+    
+  fetchAndDisplayData(`${bigHugeUrl}${bigHugeApiKey}/${searchTerm}/json`);
+
+}; /* search function that prevents pagereload, sets a few varibles and then uses abow defined functions
+ to determine what to search for and then fetches data from  the flickar and big Huge api's */
 
 searchButton.addEventListener('click',search);
