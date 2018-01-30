ea627b204337841b3a05de8bedb35299feee33a7
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-30 15:58:22 +0100

    refactor code, break functions down into smaler parts

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const searchButton = document.querySelector('button');
 const searchField = document.querySelector('input');
 const content = document.querySelector('.content');
 const list = document.querySelector('ul');
-const flickerUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search"
+const flickerUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search";
 const flickerApiKey = "2e0240e78ddd71b7a1fb75ec2a28b41c";
+const flickerApendage = "safe_search=1&format=json&nojsoncallback=1";
 const bigHugeUrl = "http://words.bighugelabs.com/api/2/";
 const bigHugeApiKey = "9b7c8ee5f8778f4292b2aebad9fed742";
 let results;
@@ -36,8 +37,33 @@ const checkForWordType = (wordType) => {
 
 };
 
+const removeContent = (container) => {
+  while(container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+};
+
+const displayResults = (results) => {
+
+  for (let i = 0; i < results.length; i++) {
+    
+    let figure = document.createElement('figure');
+    let link = document.createElement('a');
+    let img = document.createElement('img');
+
+    link.target = "_blank"
+    link.href = `https://www.flickr.com/photos/${results[i].owner}/${results[i].id}`;
+    img.src = `https://farm${results[i].farm}.staticflickr.com/${results[i].server}/${results[i].id}_${results[i].secret}.jpg`;
+
+    link.appendChild(img);
+    figure.appendChild(link);
+    content.appendChild(figure);
+  }
+};
+
 const search = function(e) {
   e.preventDefault();
+
   let searchValue;
 
   if(searchField.value) {
@@ -46,27 +72,14 @@ const search = function(e) {
     searchValue = this.innerHTML;
   }
 
-  while(content.hasChildNodes()) {
-    content.removeChild(content.firstChild);
-  }
+  removeContent(content);
 
-  fetch(`${flickerUrl}&api_key=${flickerApiKey}&text=${searchValue}&safe_search=1&format=json&nojsoncallback=1"`)
+  fetch(`${flickerUrl}&api_key=${flickerApiKey}&text=${searchValue}&${flickerApendage}`)
     .then(res => res.json()).then(res => {
     
       results = res.photos.photo;
 
-      for (let i = 0; i < results.length; i ++) {
-        let figure = document.createElement('figure');
-        let link = document.createElement('a');
-        let img = document.createElement('img');
-        link.target = "_blank"
-        link.href = `https://www.flickr.com/photos/${results[i].owner}/${results[i].id}`;
-        img.src = `https://farm${results[i].farm}.staticflickr.com/${results[i].server}/${results[i].id}_${results[i].secret}.jpg`;
-        link.appendChild(img);
-        figure.appendChild(link);
-
-        content.appendChild(figure);
-      }
+      displayResults(results);
 
       searchField.value = "";
      
@@ -79,9 +92,7 @@ const search = function(e) {
     .then(res => res.json())
     .then(res => {
 
-      while(list.hasChildNodes()) {
-        list.removeChild(list.firstChild);
-      }
+      removeContent(list);
 
       if (res.noun) {
         checkForWordType(res.noun);
@@ -90,6 +101,8 @@ const search = function(e) {
         checkForWordType(res.verb);
       }
      
+    }).catch(e => {
+      console.log(e);
     });
 };
 
